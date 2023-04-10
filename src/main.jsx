c92c8ecd38186d5a92a4e3dff16b48926edc4a7f
tanvirhasn <tanvirhasan.edu.bd@gmail.com>
2023-04-10 16:44:05 +0600

    home,blog,statistic,appliedjobs component added

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,31 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Layout from './components/Layout/Layout';
+import Statistics from './components/Statistics/Statistics';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs';
+import Blog from './components/Blog/Blog';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout></Layout>
-  }
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/statistics",
+        element: <Statistics></Statistics>
+      },
+      {
+        path: "/appliedjobs",
+        element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>
+      }
+    ]
+  },
+
+
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
